706dedbeb1881b72100e647598a3e05c0dc3fb4d
Richard Hinojosa <txhc4life@gmail.com>
2023-08-06 03:16:24 -0500

    Finished Section 3.16 Macros

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
-    println!("Hello, world!");
-
-    calculate_weight_on_mars(100.0);
+    println!("Weight on Mars: {}kg.", calculate_weight_on_mars(100.0));
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
-    50.0
+    (weight / 9.81) * 3.711
 }
