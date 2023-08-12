ffcee6b8760b45f4848a5dd571f4be5035460a20
Richard Hinojosa <txhc4life@gmail.com>
2023-08-12 01:59:40 -0500

    Finished Section 3.19 Ownership

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
+    some_fn(input);
     io::stdin().read_line(&mut input);
-    let mut mars_weight = calculate_weight_on_mars(100.0);
+    let mars_weight = calculate_weight_on_mars(100.0);
     println!("Weight on Mars: {}kg.", mars_weight);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
 }
+
+fn some_fn(s: String) {}
