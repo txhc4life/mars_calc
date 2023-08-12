6e6d2e1f5497ea0a1d0da7f771a1528645e5fd22
Richard Hinojosa <txhc4life@gmail.com>
2023-08-12 01:37:45 -0500

    Finished Section 3.18 The Standard Library.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::io;
+
 fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
     let mut mars_weight = calculate_weight_on_mars(100.0);
-    mars_weight = mars_weight * 1000.0;
-    println!("Weight on Mars: {}g.", mars_weight);
+    println!("Weight on Mars: {}kg.", mars_weight);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
